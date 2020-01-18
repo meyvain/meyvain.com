38d6fa6f74486053dfa231eeda6fe31adb3b1aa7
Tom Gurion <nagasaki45@gmail.com>
2020-01-18 17:35:33 +0000

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Meytal Mitchell Vainstub'
+SITENAME = 'Meytal Mitchell Vainstub'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+THEME = 'theme'
+DIRECT_TEMPLATES = ['index']
+AUTHOR_SAVE_AS = False
+CATEGORY_SAVE_AS = False
+TAG_SAVE_AS = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
